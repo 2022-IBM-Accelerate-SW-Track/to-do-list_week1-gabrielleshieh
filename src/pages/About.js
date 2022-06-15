b606fe81e2787f41628b6c5b1fe93fdbf674b130
“gabrielleshieh” <gabrielle_shieh@brown.edu>
2022-06-15 17:58:49 -0400

    week 1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import "./About.css";
+import gabby from "src/assets/IMG_8989.jpeg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={gabby}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Gabby Shieh</div>
+            <div className="brief_description">
+              // Hi! I'm Gabby. I am a rising junior at Brown University studying computer science. 
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
